fb539a548e36f48541035297145fe3de7a2b66f4
Sniijz <robin.cassagne@wanadoo.fr>
2024-05-05 16:20:23 +0200

    adding guitarix presets

---
+++ guitarix/banks/banklist.js
@@ -0,0 +1,22 @@
+[
+  ["Sniijz", "Sniijz.gx", 1, 0, [1, 2], 1714764091],
+  ["hk", "hk.gx", 1, 0, [1, 2], 1714678147],
+  ["running_wild", "running_wild.gx", 1, 0, [1, 2], 1714678147],
+  ["P.Z.", "P.Z..gx", 1, 0, [1, 2], 1714678147],
+  ["metalpack-v1", "metalpack-v1.gx", 1, 0, [1, 2], 1714678147],
+  ["BroomTreble", "BroomTreble.gx", 1, 0, [1, 2], 1714678147],
+  ["shiva", "shiva.gx", 1, 0, [1, 2], 1714678147],
+  ["funkmuscle", "funkmuscle.gx", 1, 0, [1, 2], 1714678147],
+  ["zettberlin", "zettberlin.gx", 1, 0, [1, 2], 1714678147],
+  ["kokoko3k", "kokoko3k.gx", 1, 0, [1, 2], 1714678147],
+  ["JP", "JP.gx", 1, 0, [1, 2], 1714678147],
+  ["StudioDave", "StudioDave.gx", 1, 0, [1, 2], 1714678147],
+  ["Darlings", "Darlings.gx", 1, 0, [1, 2], 1714678147],
+  ["silentz0r", "silentz0r.gx", 1, 0, [1, 2], 1714678147],
+  ["tombed", "tombed.gx", 1, 0, [1, 2], 1714678147],
+  ["musiclab-MarkKnopfler", "musiclab-MarkKnopfler.gx", 1, 0, [1, 2], 1714678147],
+  ["evh_tones", "evh_tones.gx", 1, 0, [1, 2], 1714678147],
+  ["Fender 65 Deluxe Reverb", "Fender 65 Deluxe Reverb.gx", 1, 0, [1, 2], 1714677684],
+  ["Fender 65 Deluxe Reverb MAX BRIGHTNESS", "Fender 65 Deluxe Reverb MAX BRIGHTNESS.gx", 1, 0, [1, 2], 1714677684],
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1714662581]
+]
